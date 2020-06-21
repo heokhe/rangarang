284fa3ffe25b4c0cd6190d368286c347e71069d0
Hosein Khansari <hrk.x11@gmail.com>
2020-06-21 23:04:22 +0430

    Improve examples/web: Get the image from the URL
    To use: move the images to examples/web/images, and head to /examples/web/?i=FILE_NAME

--- examples/web/index.js
+++ examples/web/index.js
@@ -1,21 +1,31 @@
-/* eslint-disable no-console */
+/* eslint-disable no-restricted-globals, no-console */
 import rangarang, { getDataFromCanvas } from '../../dist/index.modern.js';
 
 window.onload = () => {
-  const image = new Image();
-  image.src = 'leaf.jpg';
-  image.onload = () => {
-    console.time('picker');
-    const bestColor = rangarang(getDataFromCanvas(image)).primary;
-    console.timeEnd('picker');
-    document.body.style.backgroundColor = bestColor;
-    document.title = bestColor;
-    document.querySelector('meta[name=theme-color]').content = bestColor;
-    document.body.prepend(image);
-    requestAnimationFrame(() => {
+  const filename = new URLSearchParams(location.search).get('i');
+  if (filename) {
+    const image = new Image();
+    image.src = `images/${filename}`;
+    image.onload = () => {
+      console.time('picker');
+      const bestColor = rangarang(getDataFromCanvas(image)).primary;
+      console.timeEnd('picker');
+      document.body.prepend(image);
       requestAnimationFrame(() => {
-        image.classList.add('active');
+        requestAnimationFrame(() => {
+          image.classList.add('active');
+          document.body.style.backgroundColor = bestColor;
+          document.title = bestColor;
+          document.querySelector('meta[name=theme-color]').content = bestColor;
+        });
       });
-    });
-  };
+    };
+  } else {
+    document.body.insertAdjacentHTML('beforeend', `
+      <span>
+        Add a picture to <b>/examples/web</b>,
+        and use the <b>?i=&lt;file-name&gt;</b> query string.
+      </span>
+    `);
+  }
 };
